6fbdb7d0b514c633333b3c1861e2cb326f6ff305
Elias Ventura <eliasventura23@gmail.com>
2019-03-04 17:10:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,13 +13,13 @@ def piglatinize_word(word)
   vowel = ["a", "e", "i", "o", "u"]
   consonants = alphabet - vowel
 
-  if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+  if consonants.include?(word[0].downcase) && consonants.include?(word[1]) && consonants.include?(word[2])
     word[3..-1] + word[0..2] + "ay"
 
-  elsif consonants.include?(word[0]) && consonants.include?(word[1])
+  elsif consonants.include?(word[0].downcase) && consonants.include?(word[1])
     word[2..-1] + word[0..1] + "ay"
 
-  elsif consonants.include?(word[0])
+  elsif consonants.include?(word[0].downcase)
     word[1..-1] + word[0] + "ay"
 
 
